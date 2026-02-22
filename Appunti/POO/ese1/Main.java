d5b129d4aa72ec0a61ffb76a713c9aef0a8647f7
sandie69p <salvatore.pisu04@gmail.com>
2026-02-22 16:30:35 +0000

    Auto Sync: 2026-02-22 16:30:35

--- Appunti/POO/ese1/Main.java
+++ Appunti/POO/ese1/Main.java
@@ -2,16 +2,16 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.print("Enter you payout of shopping: ");
 		String shoppingBills = System.console().readLine();
-		shoppingBills.toInt();
+		float floatedBills = Float.parseFloat(shoppingBills);
 
-		System.out.println("Bill: ${shoppingBills}");
+		System.out.println("Bill: " + floatedBills);
 
-		if(shoppingBills >= 50 && shoppingBills <= 100) {
-			shoppingBills -= shoppingBills * 0.10;
-		} else if(shoppingBills > 100) {
-			shoppingBills -= shoppingBills * 0.20;
+		if(floatedBills >= 50 && floatedBills <= 100) {
+			floatedBills -= floatedBills * 0.10;
+		} else if(floatedBills > 100) {
+			floatedBills -= floatedBills * 0.20;
 		}
 
-		System.out.println("After discount: ${shoppingBills}");
+		System.out.println("After discount: " + floatedBills);
 	}
 }
