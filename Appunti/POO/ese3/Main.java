115cc2d9415826cf891a44b126c069cac9b69966
sandie69p <salvatore.pisu04@gmail.com>
2026-02-23 19:19:31 +0000

    Auto-Sync: 2026-02-23 19:19:31

--- Appunti/POO/ese3/Main.java
+++ Appunti/POO/ese3/Main.java
@@ -1,5 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    
+    System.out.print("inserisci una temperatura celsius °C: ");
+    double celsius = System.console();
+
+    parseDouble(celsius);
+
+    double fahrenheit = (celsius * 9 / 5) + 32;
+
+    System.out.println("Temperatura fahrenheit °F: " + fahrenheit);
   }
 }
