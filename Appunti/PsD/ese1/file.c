f7e8cdb135953959aa98aaa2e3fa361522ea353b
sandie69p <salvatore.pisu04@gmail.com>
2026-02-25 10:28:52 +0100

    Auto-Sync: 2026-02-25 10:28:52

--- Appunti/PsD/ese1/file.c
+++ Appunti/PsD/ese1/file.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
 
 int somma(int, int);
 
 int main() {
-	
-}
-
-int somma(int n, int m) {
-	return n + m;
+	boolean ordinato = false;	
 }
