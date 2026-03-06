311a8900d21457604e8b0c8b4957afa970ef1269
sandie69p <salvatore.pisu04@gmail.com>
2026-03-06 12:11:43 +0100

    Auto Sync (Silent): 2026-03-06 12:11:43

--- Appunti/PsD/ese2/file.c
+++ Appunti/PsD/ese2/file.c
@@ -5,9 +5,31 @@
 typedef struct {
     char *titolo;
     char *autore;
-    int anno;
+    int pagine;
 } Libro;
 
+void aggiungiLibro(int *);
+void rimuoviLibro(int *);
+void mostraLibro(int *);
+
 int main(void) {
-	
+	int count;
+	int choise;
+
+	printf("Quanti Libri vuoi aggiungere? ");
+	scanf("%d", &count);
+
+	Libro *p = (Libro *)malloc(count * sizeof(Libro));
+
+	while(1) {
+		printf("Inserisci un numero da 0 a 4:\n0: Esci dall'applicazione;\n1: Aggiungi Libro;\n2: Rimuovi Libro;\n3: Mostra Libri;\n ");
+		scanf("%d", &choise);
+		switch(choise) {
+			case 0: { } break;
+			case 1: { aggiungiLibro(p);  } break;
+			case 2: { rimuoviLibro(p);  } break;
+			case 3: { mostraLibro(p);  } break;
+			default: continue;
+		}
+	}
 }
